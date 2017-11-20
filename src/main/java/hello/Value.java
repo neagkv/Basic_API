8644c99b3d1909adad6af6910d1c4f7dfa9a2d9f
Kevin Neag <neagkv@gmail.com>
2017-11-19 20:11:43 -0500

    first comment in repo

---
+++ src/main/java/hello/Value.java
@@ -0,0 +1,42 @@
+package hello;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+//ignore unknown JSON values that do not have specific variables
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Value {
+
+        //values from the JSON object
+        private Long id;
+        private String quote;
+
+        //constructor
+        public Value() {
+        }
+
+        //getters/setters
+        public Long getId() {
+            return this.id;
+        }
+
+        public String getQuote() {
+            return this.quote;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public void setQuote(String quote) {
+            this.quote = quote;
+        }
+
+        @Override
+        public String toString() {
+            return "Value{" +
+                    "id=" + id +
+                    ", quote='" + quote + '\'' +
+                    '}';
+        }
+    }
+
